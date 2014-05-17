c4c92420a9d6562b425905ebd8ac26e16f2596c7
Justin Worthe <justin.worthe@gmail.com>
2014-05-18 00:32:28 +0200

    Fixed preloader gif location

--- js/game.js
+++ js/game.js
@@ -94,7 +94,7 @@ function Boot() {
 
 Boot.prototype = {
   preload: function() {
-    this.load.image('preloader', 'assets/preloader.gif');
+    this.load.image('preloader', 'assets/images/preloader.gif');
   },
   create: function() {
     this.game.input.maxPointers = 1;
